ddaa7b7cc2edc6b0bd0830d91f32da5bc0eae27b
Michel Keijzers <michelkeijzers@hotmail.com>
2021-12-20 23:54:24 +0100

    Hint file, reformatted HTML

--- RelayBox/RelayBox/RelayBoxServer.cpp
+++ RelayBox/RelayBox/RelayBoxServer.cpp
@@ -26,34 +26,42 @@ AsyncWebServer server(80);
 
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
 <head>
-  <title>ESP Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
+    <title>ESP Web Server 0.01</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
+    <style>
+        html {font-family: Arial; display: inline-block; text-align: center;}
+        h2 {font-size: 3.0rem;}
+        p {font-size: 3.0rem;}
+        body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+        .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+        .switch input {display: none}
+        .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+        .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+        input:checked+.slider {background-color: #b30000}
+        input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+    </style>
 </head>
+
 <body>
-  <h2>ESP Web Server</h2>
-  %BUTTONPLACEHOLDER%
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
-  xhr.send();
-}
-</script>
+    <h2>ESP Web Server 0.01</h2>
+    %BUTTONPLACEHOLDER%
+
+    <script>function toggleCheckbox(element) {
+        var xhr = new XMLHttpRequest();
+        if(element.checked)
+        {
+            xhr.open("GET", "/update?output="+element.id+"&state=1", true); 
+        }
+        else
+        { 
+            xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+            xhr.send();
+        }
+    </script>
 </body>
 </html>
 )rawliteral";
@@ -86,14 +94,28 @@ RelayBoxServer::RelayBoxServer()
     if (var == "BUTTONPLACEHOLDER") 
     {
         STRING buttons = "";
-        buttons += "<h4>Output - GPIO 2</h4><label class = \"switch\"><input type=\"checkbox\" onchange = \"toggleCheckbox(this)\" id=\"2\" " + OutputState(2) + "><span class = \"slider\"></span></label>";
-        buttons += "<h4>Output - GPIO 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange = \"toggleCheckbox(this)\" id=\"4\" " + OutputState(4) + "><span class = \"slider\"></span></label>";
-        buttons += "<h4>Output - GPIO 33</h4><label class=\"switch\"><input type=\"checkbox\" onchange = \"toggleCheckbox(this)\" id=\"33\" " + OutputState(33) + "><span class = \"slider\"></span></label>";
+        buttons += 
+            "<h4>Output - GPIO 2</h4>" \
+            "    <label class = \"switch\">" \
+            "        <input type = \"checkbox\" onchange = \"toggleCheckbox(this)\" id=\"2\" " + OutputState(2) + ">" \
+            "        <span class = \"slider\"/>" \
+            "    </label>" \
+            "" \
+            "<h4>Output - GPIO 4</h4>" \
+            "    <label class = \"switch\">"\
+            "        <input type = \"checkbox\" onchange = \"toggleCheckbox(this)\" id=\"4\" " + OutputState(4) + ">" \
+            "        <span class = \"slider\"/>" \
+            "    </label>" \
+            "" \
+            "<h4>Output - GPIO 33</h4>" \
+            "    <label class = \"switch\">" \
+            "        <input type = \"checkbox\" onchange = \"toggleCheckbox(this)\" id=\"33\" " + OutputState(33) + ">" \
+            "    <span class = \"slider\"/>" \
+            "    </label>";
         return buttons;
     }
     return STRING();
 }
-
 
 void RelayBoxServer::Begin()
 {
@@ -106,16 +128,20 @@ void RelayBoxServer::Begin()
     server.on("/update", HTTP_GET, [](AsyncWebServerRequest* request) {
         STRING inputMessage1;
         STRING inputMessage2;
+
         // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-        if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
+        if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) 
+        {
             inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
             inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
             digitalWrite((uint8_t)inputMessage1.toInt(), inputMessage2.toInt());
         }
-        else {
+        else 
+        {
             inputMessage1 = "No message sent";
             inputMessage2 = "No message sent";
         }
+
         Serial.print("GPIO: ");
         Serial.print(inputMessage1);
         Serial.print(" - Set to: ");
